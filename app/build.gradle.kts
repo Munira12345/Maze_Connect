e95a58552c75c8d4646922a14ffef05abef377c6
Munira12345 <munirangomo@gmail.com>
2024-07-04 22:47:49 +0300

    dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,49 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-}
+    //core dependencies
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //for UI
+        implementation("androidx.compose.ui:ui:1.5.0")
+        implementation("androidx.compose.material:material:1.5.0")
+        implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
+        debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
+        implementation("androidx.activity:activity-compose:1.8.0")
+    //lifecycle
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+
+  //navigation
+        implementation("androidx.navigation:navigation-compose:2.7.1")
+//livedata
+        implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+
+//coroutine support
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+//hilt dependency injection
+        implementation("com.google.dagger:hilt-android:2.47")
+        kapt("com.google.dagger:hilt-android-compiler:2.47")
+        implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
+
+    //retrofit
+            implementation("com.squareup.retrofit2:retrofit:2.9.0")
+            implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+   //room local database
+        implementation("androidx.room:room-runtime:2.5.2")
+        kapt("androidx.room:room-compiler:2.5.2")
+        implementation("androidx.room:room-ktx:2.5.2")
+
+
+
+
+
+
+
+
+    }
