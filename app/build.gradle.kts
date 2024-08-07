4d33e1852178e3b6838a21eef048c009d2ac01e4
Munira12345 <munirangomo@gmail.com>
2024-08-07 10:39:45 +0300

    json file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("com.android.application")
-    id("com.google.gms.google-services")
+      id("com.google.gms.google-services")
 }
 
 android {
